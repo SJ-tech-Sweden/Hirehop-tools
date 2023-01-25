eca57bde97d79bfe4dcd3c7df8b52a130542444c
Samuel Sjöbergsson <samuel@sjobergsson.com>
2023-01-25 15:58:40 +0100

    Added logging to file

--- hirehop/webhook/views.py
+++ hirehop/webhook/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+import yaml
+
+#Logging to a speciefied file
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                    filename='/app/logs/webhook.log', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
+
+with open('/app/hirehopScanning/config.yaml') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+api_token = config['hirehop']['api_token']
 # Create your views here.
 
 def new_job(request):
@@ -8,6 +19,7 @@ def new_job(request):
         data = request.POST.dict()
         # process the webhook data here
         print(data)
+        logging.info(info)
         return JsonResponse({'status': 'success'})
     else:
         return JsonResponse({'error': 'Invalid request method'})
