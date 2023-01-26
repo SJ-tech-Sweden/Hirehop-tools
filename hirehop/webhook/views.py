89d0c8af028e0c8cf9c32af31c62c6a21f8feb7a
Samuel Sjöbergsson <samuel@sjobergsson.com>
2023-01-26 21:27:26 +0100

    Webhooks sharepoint start

--- hirehop/webhook/views.py
+++ hirehop/webhook/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from datetime import date
+
+from shareplum import Site
+from shareplum import Office365
+from shareplum.site import Version
+
 import yaml
+import json
 
 #Logging to a speciefied file
 import logging
@@ -13,7 +20,16 @@ with open('/app/hirehopScanning/config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
 api_token = config['hirehop']['api_token']
-# Create your views here.
+
+#Sharepoint config
+sharepoint_user = config['sharepoint']['username']
+sharepoint_password = config['sharepoint']['password']
+sharepoint_site = config['sharepoint']['site']
+sharepoint_library = config['sharepoint']['document_library']
+
+authcookie = Office365(sharepoint_site, username=sharepoint_user, password=sharepoint_password).GetCookies()
+site = Site(sharepoint_site, version=Version.v365, authcookie=authcookie)
+
 
 
 @csrf_exempt
@@ -21,11 +37,23 @@ def new_job(request):
     if request.method == 'POST':
         data = request.POST.dict()
         data_raw = request.body
+        data_json = json.loads(request.body)
+
+        job_data = data_json['data']
+        job_name = job_data['JOB_NAME']
+
+        current_year = date.today().year
+
+        create_sharepoint_folder = bool(job_data['CUSTOM_FIELDS']['sharepoint_project']['value'])
+
+        if create_sharepoint_folder:
+
+            folder = site.Folder('{}/{}/{}'.format(sharepoint_library, current_year, job_name))
+
         # process the webhook data here
-        print(data)
-        logging.info(data)
-        logging.info(data_raw)
-        return JsonResponse({'status': 'success'})
+        logging.info(data_json)
+        print(create_sharepoint_folder)
+        return JsonResponse({'status': 'success', 'data': data_json['data']})
     else:
         logging.info('No data')
         return JsonResponse({'error': 'Invalid request method'})
