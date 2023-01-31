41fc4c9c5a1b64c54a5c6757e1f31afe09bd04ac
Samuel Sjöbergsson <33666789+sjobergsson@users.noreply.github.com>
2023-01-31 14:27:56 +0100

    update form

--- hirehop/sound/forms.py
+++ hirehop/sound/forms.py
@@ -4,6 +4,11 @@ import requests
 
 from .models import channel_lists
 
+#Logging to a speciefied file
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                    filename='/app/logs/sound.log', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
+
 #Open configuration file
 with open('/app/hirehopScanning/config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
@@ -26,4 +31,17 @@ def get_mixers():
     except:
         mixers = {}
 
-    return mixers
+    logging.info(mixers)
+
+    return "{ ilive: 134 }"
+
+class ChannelListForm(forms.Form):
+    channel_list_name = forms.CharField(max_length=100)
+    projectID = forms.CharField(max_length=30)
+    mixerID = forms.ChoiceField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        mixers = get_mixers()
+        mixer_choices = [(key, value) for key, value in mixers.items()]
+        self.fields['mixerID'].choices = mixer_choices
