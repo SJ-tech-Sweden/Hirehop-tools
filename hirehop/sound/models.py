a042ca173914c11dd820993a6134997648ed3244
Samuel Sjöbergsson <33666789+sjobergsson@users.noreply.github.com>
2023-02-01 11:01:43 +0100

    Models

--- hirehop/sound/models.py
+++ hirehop/sound/models.py
@@ -29,15 +29,15 @@ class channel_lists(models.Model):
 class channel_list_inputs(models.Model):
 
     # Fields
-    musician = models.TextField(max_length=100)
+    musician = models.TextField(max_length=100, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True, editable=False)
-    notes = models.TextField(max_length=500)
+    notes = models.TextField(max_length=500, blank=True, null=True)
     last_updated = models.DateTimeField(auto_now=True, editable=False)
-    instrument = models.TextField(max_length=100)
-    stage_input = models.TextField(max_length=100)
+    instrument = models.TextField(max_length=100, blank=True, null=True)
+    stage_input = models.TextField(max_length=100, blank=True, null=True)
     console_channel = models.IntegerField(blank=True, null=True)
     channel_list = models.ForeignKey(channel_lists, on_delete=models.CASCADE)
-    mic_di = models.TextField(max_length=100)
+    mic_di = models.TextField(max_length=100, blank=True, null=True)
 
     class Meta:
         pass
@@ -57,14 +57,14 @@ class channel_list_outputs(models.Model):
 
     # Fields
     last_updated = models.DateTimeField(auto_now=True, editable=False)
-    instrument = models.TextField(max_length=100)
-    person = models.TextField(max_length=100)
-    output_type = models.TextField(max_length=100)
+    instrument = models.TextField(max_length=100, blank=True, null=True)
+    person = models.TextField(max_length=100, blank=True, null=True)
+    output_type = models.TextField(max_length=100, blank=True, null=True)
     console_output = models.IntegerField(blank=True, null=True)
-    notes = models.TextField(max_length=500)
+    notes = models.TextField(max_length=500, blank=True, null=True)
     channel_list = models.ForeignKey(channel_lists, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True, editable=False)
-    mix = models.TextField(max_length=100)
+    mix = models.TextField(max_length=100, blank=True, null=True)
 
     class Meta:
         pass
