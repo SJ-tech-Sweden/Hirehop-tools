118612579db7d7d9622da1f3efaaf6dbc1a83bd7
Samuel Sjöbergsson <33666789+sjobergsson@users.noreply.github.com>
2023-02-03 10:35:00 +0100

    update

--- hirehop/sound/forms.py
+++ hirehop/sound/forms.py
@@ -21,6 +21,8 @@ with open('/app/hirehopScanning/config.yaml') as f:
 
 api_token = config['hirehop']['api_token']
 mixer_category = config['hirehop']['categories']['mixers']
+mic_category = config['hirehop']['categories']['microphones']
+di_category = config['hirehop']['categories']['di']
 
 def get_mixers():
     url = "https://myhirehop.com/modules/stock/list.php?rows=400&page=1&token={}&_search=true&head={}".format(api_token, mixer_category)
@@ -51,6 +53,36 @@ def get_mixers():
 
     return mixers_result
 
+
+def get_mics():
+    url = "https://myhirehop.com/modules/stock/list.php?rows=400&page=1&token={}&_search=true&head={}".format(api_token, microphone_category)
+
+    payload={}
+    headers={}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    mics = {}
+
+    try:
+        mics = json.loads(response.text)['rows']
+    except:
+        mics = {}
+
+    logging.info(mics)
+    logging.info('---------------')
+
+
+    mics_result = [(item['id'], item['cell']['TITLE']) for item in mics]
+
+    #mixers_dict = {row['ID']: row['TITLE'] for index, row in df.iterrows()}
+
+
+    logging.info('--- Result ---')
+    logging.info(mics_result)
+
+    return mics_result
+
 class ChannelListsForm(ModelForm):
     Name = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'class': 'form-control'}))
     projectID = forms.CharField(widget=forms.HiddenInput)
@@ -75,13 +107,20 @@ class ChannelListsForm(ModelForm):
 
 
 class ChannelListInputForm(forms.ModelForm):
+    musician = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    notes = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    instrument = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    stage_input = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    console_channel = forms.CharField(max_length=100, label='', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    mic_di = forms.ChoiceField(choices=(get_mixers()), label='', widget=forms.Select(attrs={'class': 'form-select'}))
+
     class Meta:
         model = channel_list_input
         fields = [
-            "musician",
-            "notes",
-            "instrument",
             "stage_input",
             "console_channel",
+            "instrument",
             "mic_di",
+            "musician",
+            "notes",
         ]
