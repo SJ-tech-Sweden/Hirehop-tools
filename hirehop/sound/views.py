7ead13d331c679c64b13ddc41d0eac1b1b499204
Samuel Sjöbergsson <33666789+sjobergsson@users.noreply.github.com>
2023-03-09 09:38:28 +0100

    Update

--- hirehop/sound/views.py
+++ hirehop/sound/views.py
@@ -211,7 +211,7 @@ def edit_channellist(request):
                 messages.info(request, job_nr)
                 messages.info(request, cd)
                 messages.info(request, channel_list_input_obj.__dict__)
-                if cd.get("mic_di") != 0:
+                if cd.get("mic_di") != 0 and not channel_list_input_obj.mic_di == cd.get("mic_di"):
                     #add_equipment(request, cd.get('projectID'), cd.get('mixerID'))
                     messages.info(request, "Add mic to hirehop")
                 form_input.save()
